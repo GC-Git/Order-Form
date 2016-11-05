3faa2bd83dc046e5b055c9407b1116ce2cb7e6e0
Gordon Clark <Gordon2108@gmail.com>
2016-11-05 16:51:02 -0700

    Added error handling

--- OrderFormMain.py
+++ OrderFormMain.py
@@ -1,62 +1,86 @@
 # Program: Lesson 3a Order Form
 # Programmer: Gordon Clark
 # Date: Oct 10, 2016
-# Program purpose: The programs purpose is to list total cost of a given number of items. Each items with different costs and quantity.
+# Program purpose: The programs purpose is to list total cost of a given number of items.
 
 
-#Variable Definitions
+# Variable Definitions
 import locale
 locale.setlocale ( locale.LC_ALL, '')
 username = 'Meatbag'
-item_cnt = int(0)                                       #Item Counter
-order_total = float(0.0)                                #total for the order
+item_cnt = int(0)  # Item Counter
+order_total = float(0.0)  # total for the order
 item_price = float(3.5)
 valid_data = False
 item_total = 0
-#End of variable definitions
+# End of variable definitions
 
 username = input("What is your name? ")
 
+
+## MAIN LOOP REQUEST ##
 cont = "" 
 while cont.lower() != 'y' and cont.lower() != 'n':
-    cont = input('Would you like to place an order? (y/n)') #Sentinal
+    cont = input('Would you like to place an order? (y/n)')
+## END MAIN LOOP REQUEST ##
 
+
+## MAIN LOOP ##
 while cont.lower() == 'y':
+
+    # valid_data acts as a sentinel for the next sub-loop within the main loop. It must be set to
+    # true to go to the next loop
     valid_data = False
     
     while not valid_data:
-        # Cookie list #
         print("Please choose one of our flavors. Enter the item number to make a selection.")
         print("Number\tFlavor")
         print("1.\tSavannahs")
-        print("2.\tThin Mints")
+        print("2.\tThin Mints")  # Hint: \t in a string is tab
         print("3.\tTag-a-longs")
-        print() #blank line
-        item = input("Enter item number>") #Entering as string
-        #End of Cookie List#
+        print()
 
-        if item == '1' or item == '2' or item == '3':
-            valid_data = True
+        try:
+            item = input("Enter item number>")  # Hint: item resolves to a string, not an INT.
+
+        except Exception as inst:
+            print("error: ", inst)
+
         else:
-            print("\nThat is not an option. I strongly suggest you rethink the choices that have led you to this point in your life.")
+            if item == '1' or item == '2' or item == '3':  # item must be a string, or converted to a string
+                valid_data = True
+            else:
+                print('\nThat is not an option. I strongly suggest you rethink the'
+                      ' choices that have led you to this point in your life.')
+                print()
 
+    # Flag reset
     valid_data = False
 
+    ## REQUEST AMOUNT ##
     while not valid_data:
-        item_cnt = int(input("How many would you like? "))
+        try:
+            item_cnt = int(input("How many would you like? "))
 
-        if 1 <= item_cnt <= 10:
-            valid_data = True
+        except Exception as inst:
+            print("error: ", inst)
+
         else:
-            print("Calm down buddy you dont need THAT many cookies.")
+            if 1 <= item_cnt <= 10:
+                valid_data = True
+            else:
+                print("Calm down buddy you don't need THAT many cookies.")
 
-    item_total = item_cnt * item_price
-    fmt_total = locale.currency(item_total, grouping = True)
+            ## ITEM TOTAL + FORMAT ##
+            item_total = item_cnt * item_price
+            fmt_total = locale.currency(item_total, grouping=True)
+            ## END ITEM TOTAL + FORMAT ##
+    ## END REQUEST AMOUNT ##
 
-    # Determine cookie name for output
-    if item == 1:
+    ## DETERMINE COOKIE TYPE + PRINT ##
+    if item == '1':
         name = "Savannah"
-    elif item == 2:
+    elif item == '2':
         name = "Thin Mints"
     else:
         name = "Tag-a-Longs"
@@ -64,27 +88,44 @@ while cont.lower() == 'y':
     print("Order for {}.".format(username))
     print("Item\tPrice\tQty\tTotal")
     print("\n{}    {}    {}   {}".format(name,item_price,item_cnt,fmt_total))
-    print() # blank line
-    
+    print()
+    ## END DETERMINE COOKIE TYPE + PRINT ##
+
+    # Flag Reset
     valid_data = False
 
+    ## INPUT CONFIRMATION ##
     while not valid_data:
-        incl = input("Would you like to add this to your order?(y/n)")
-        print()
-        if incl.lower() == 'y':
-            order_total = order_total + item_total
-            item_cnt = item_cnt + 1
-            valid_data = True
-            print("{} was added to your order.".format(name))
-        elif incl.lower() == 'n':
-            print("{} was not added to your total.".format(name))
-            valid_data == True
-        else:
-            print("what? WHAT? I CANT UNDERSTAND YOU!")
-        print()
-    cont = input("Would you like to add another order?")
-#End main loop
+        try:
+            incl = input("Would you like to add this to your order?(y/n) ")
+            print()
 
-fmt_total = locale.currency(item_total, grouping = True)
-print("You have ordered {} items costing {}".format(item_cnt,fmt_total))
+        except Exception as detail:
+            print("error: ", detail)
+
+        else:
+            if incl.lower() == 'y':
+                order_total += item_total
+                item_cnt += 1
+                valid_data = True
+                print("{} was added to your order.".format(name))
+            elif incl.lower() == 'n':
+                print("{} was not added to your total.".format(name))
+                valid_data = True
+            else:
+                print("what? WHAT? I CANT UNDERSTAND YOU!")
+            print()
+    ## END INPUT CONFIRMATION ##
+    try:
+        cont = ""
+        while cont.lower() != 'y' and cont.lower() != 'n':
+            cont = input("Would you like to add another order?")
+    except Exception as detail:
+        print("error: ", detail)
+## END MAIN LOOP ##
+
+## FINAL SALE DISPLAY ##
+fmt_total = locale.currency(item_total, grouping=True)
+print("You have ordered {} items costing {}".format(item_cnt, fmt_total))
 print("Thank you for using this program. Press any key to exit.")
+## END FINAL SALE DISPLAY ##
